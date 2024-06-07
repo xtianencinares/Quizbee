d385fc8d20f8032c990534030d7aa3795747ad91
Christian Encinares Bocz <144127538+xtianencinares@users.noreply.github.com>
2024-06-07 16:44:59 +0800

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Quiz {
     public static void main(String[] args) {
-        // Questions and Answers v1.10 Agoo inc.
+        // Questions and Answers v1.1 Agoo inc.
         String[] questions = {
             "What is the capital of France?",
             "What is the largest planet in our solar system?",
